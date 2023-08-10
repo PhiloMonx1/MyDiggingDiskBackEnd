c2238a4b388462605aa13c1f8ce5a1620a979369
LEE TAE MIN <philomonx1@gmail.com>
2023-08-10 16:23:53 +0900

    Feat : DiskResponseDto 소유자 id, 닉네임 필드 추가 및 반영

--- src/main/java/side/mimi/mdd/restApi/Disk/dto/response/DiskResponseDto.java
+++ src/main/java/side/mimi/mdd/restApi/Disk/dto/response/DiskResponseDto.java
@@ -19,6 +19,8 @@ public class DiskResponseDto {
 	private Boolean isPrivate;
 	private Boolean isFavorite;
 	private Integer likeCount;
+	private Long diskOwnerId;
+	private String diskOwnerNickname;
 	private Boolean isMine;
 	//TODO: 이미지
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
