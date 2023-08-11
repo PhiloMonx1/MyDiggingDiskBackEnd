454ccce8bf3ff81c3fff4e19adf263da20bb8ccd
LEE TAE MIN <philomonx1@gmail.com>
2023-08-11 15:17:22 +0900

    Feat : DiskImgEntity 및 dto 레포지토리 선언

---
+++ src/main/java/side/mimi/mdd/restApi/Disk/dto/DiskImgDto.java
@@ -0,0 +1,21 @@
+package side.mimi.mdd.restApi.Disk.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class DiskImgDto {
+	private Long imgId;
+	private String imgUrl;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime createdAt;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime modifiedAt;
+}
